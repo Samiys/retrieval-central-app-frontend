0f030474ad41bc1f2d9dc4a4f4a7d36e95a398b0
Sami Younas <samiys005@gmail.com>
2023-07-22 04:15:23 +0500

    updates on package json

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 // vue.config.js
 module.exports = {
-  runtimeCompiler: true,
-  chainWebpack: (config) => {
-    if (process.env.NODE_ENV === 'production') {
-      // Disable CSS minification
-      config.optimization.minimize(false);
-    }
-  },
+  runtimeCompiler: true
 };
