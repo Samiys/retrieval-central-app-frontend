102f72386dc5b337a22a10ac5e6b6adf9708787c
Sami <samiys005@gmail.com>
2024-03-20 00:45:29 +0100

    getting stores data

--- src/assets/common/store.service.js
+++ src/assets/common/store.service.js
@@ -5,4 +5,10 @@ export const StoreService = {
     getStoresInfo() {
         return axios.get(`${prefix}/getStoresInfo`);
     },
+    getDistributorStoresInfo() {
+        return axios.get(`${prefix}/getDistributorStoresInfo`);
+    },
+    getMerchantStoresInfo() {
+        return axios.get(`${prefix}/getMerchantStoresInfo`);
+    },
 }
